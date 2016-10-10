ba2b16d695cd0c6d1bb2ae153d0edfbc98f4464d
kingcc <laikinfox@gmail.com>
2016-10-10 14:08:20 +0800

    script auto commit at [2016-10-10][14:08:20.638852459]

--- controller.js
+++ controller.js
@@ -32,7 +32,7 @@ function addControllers(router) {
 
 module.exports = function (dir) {
     let
-        controllers_dir = dir || 'controllers', // 如果不传参数，扫描目录默认为'controllers'
+        controllers_dir = dir || 'controllers',
         router = require('koa-router')();
     addControllers(router, controllers_dir);
     return router.routes();
